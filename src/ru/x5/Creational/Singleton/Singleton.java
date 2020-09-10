fd7122682bd129a35ef2ce14991a4ecd778a2a4f
Ivan Dmitriev <iv.dmitriev@x5.ru>
2020-09-10 09:44:37 +0300

    added Singleton

---
+++ src/ru/x5/Creational/Singleton/Singleton.java
@@ -0,0 +1,17 @@
+package ru.x5.Creational.Singleton;
+
+public class Singleton {
+  private static Singleton instance;
+
+  private Singleton() {
+
+  }
+
+  public static Singleton getInstance() {
+    if (instance == null) {
+      instance = new Singleton();
+    }
+    return instance;
+  }
+
+}
